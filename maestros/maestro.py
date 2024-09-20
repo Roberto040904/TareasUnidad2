43e3ea87c1aff2e05e366767c2bef07a64de10ae
Roberto <robertofrutos0409@gmail.com>
2024-09-20 12:10:20 -0600

    tarea 9

---
+++ maestros/maestro.py
@@ -0,0 +1,21 @@
+"""
+    numeroControl
+    nombre
+    apellido
+    rfc
+    sueldo
+"""
+
+class Maestro:
+    nombre: str
+    apellido: str
+    rfc: str
+    sueldo: float
+    
+    def __init__(self,nombre: str, apellido: str, rfc: str, sueldo: float):
+
+        self.nombre = nombre
+        self.apellido = apellido
+        self.rfc = rfc
+        self.sueldo = sueldo
+        
--- main.py
+++ main.py
@@ -1,99 +1,63 @@
-"""
-- Pacientes
-- Médicos
-- Consultas
-- Medicamentos
-"""
+from escuela.escuela import Escuela
+from estudiantes.estudiante import Estudiante
+from datetime import datetime
+from maestros.maestro import Maestro
 
-from medico.medico import Medico
-from paciente.paciente import Paciente
-from hospital.hospital import Hospital
-
-hospital = Hospital()
-
-#paciente = Paciente("Juan", 2009, 76, 1.78, "Avenida Madero") # 5
-#paciente_dos = Paciente("Jonathan", 1992, 70, 1.90, "Avenida Madero") # 5
-
-#medico = Medico("Alberto", 1900, "ALB4817BNDDDF", "Av. Periodismo") # 8
-
-#hospital.registrar_paciente(paciente=paciente)
-#hospital.registrar_paciente(paciente=paciente_dos)
-
-#hospital.registrar_medico(medico=medico)
-
-#hospital.mostrar_pacientes_menores()
-
-#hospital.mostrar_pacientes_mayores()
-
-#hospital.mostrar_medicos()
-#hospital.mostrar_pacientes()
-
-#hospital.registrar_consulta(id_paciente=1, id_medico=1)
-
+escuela = Escuela()
 
 while True:
-    print("\n*** BIENVENIDO ***")
-    print("Opciones en el Sistema")
-    print("1. Registrar paciente")
-    print("2. Registrar medico")
-    print("3. Eliminar paciente")
-    print("4. Eliminar medico")
-    print("5. Mostrar medicos")
-    print("6. Mostrar paciente")
-    print("7. Mostrar pacientes menores de edad")
-    print("8. Mostrar pacientes mayores de edad")
-    print("9. Salir")
-
-    opcion = input("Ingresa la opción que deseas: ")
+    print("*** MINDBOX ***")
+    print("1. Regitrar estudiante")
+    print("2. Regitrar maestro")
+    print("3. Regitrar materia")
+    print("4. Regitrar grupo")
+    print("5. Regitrar horario")
+    print("6. Salir")
+    
+    
+    opcion = input("Ingresa una opcion para continuar: ")
     
     if opcion == "1":
-        print("***REGISTRAR PACIENTE***")
-        
-        nombre = input("Ingresa el nombre: ")
-        ano_nacimiento = int(input("Ingresa el año de nacimiento: "))
-        peso = float(input("Ingresa peso: "))
-        estatura = float(input("Ingresa estatura: "))
-        direccion = input("Ingresa direccion: ")
-        
-        paciente = Paciente(nombre=nombre, ano_nacimiento=ano_nacimiento, peso=peso, estatura=estatura, direccion=direccion)
-        hospital.registrar_paciente(paciente=paciente)
-        print("\nPaciente registrado")
+        print("Seleccionaste la opcion de registrar")
         
         
+        numero_control = escuela.generar_numero_control()
+        print(numero_control)
+        nombre = input("Ingresa el nombre del estudiante: ")
+        apellido = input("Ingresa el apellido del estudiante: ")
+        curp = input("Ingresa el curp del estudiante: ")
+        ano = int(input("Ingresa el año de nacimiento del estudiante: "))
+        mes = int(input("Ingresa el mes de nacimiento del estudiante: "))
+        dia = int(input("Ingresa el dia de nacimiento del estudiante: "))
+        fecha_nacimiento = datetime(ano, mes, dia)
+        
+        estudiante = Estudiante(nombre, apellido, curp, fecha_nacimiento, numero_control)
+        escuela.registrar_estudiante(estudiante)
+
+    
     elif opcion == "2":
-        nombre = input("Ingresa el nombre del medico: ")
-        ano_nacimiento = int(input("Año de nacimiento del medico: "))
-        rfc = input("Ingresa el peso del medico: ")
-        direccion = input("Ingresa la direccion del medico: ")
+        print("Seleccionaste la opcion registrar maestro")
+        nombre = input("Ingresa nombre del maestro")
+        apellido = input("Ingresa apellido del maestro")
+        rfc = input("Ingresa rfc del maestro")
+        sueldo = int(input("Ingresa sueldo del maestro"))
         
-        medico = Medico(nombre = nombre, ano_nacimiento=ano_nacimiento, rfc=rfc, direccion=direccion)
-        hospital.registrar_medico(medico=medico)
-        print("\nMedico registrado")
-    
-    
-    if opcion == "3":
-        id_a_eliminar = int(input("Ingresa el ID del paciente a eliminar: "))
-        hospital.eliminar_paciente(id_a_eliminar)
-        print("Paciente eliminado correctamente")
+        numero_control_maestro = escuela.generar_numero_control_maestro(nombre,rfc)
+        print(numero_control_maestro)
         
+        maestro = Maestro(nombre,apellido, rfc, sueldo)
+        escuela.registrar_maestro(maestro)
+    
+    elif opcion == "3":
+        pass
+    
     elif opcion == "4":
-        id_medico_a_eliminar = int(input("Ingresa el ID del medico a eliminar: "))
-        hospital.eliminar_medico(id_medico_a_eliminar)
-        print("Medico eliminado correctamente")
+        pass
     
     elif opcion == "5":
-        hospital.mostrar_medicos()
-        
+        pass
+    
     elif opcion == "6":
-        hospital.mostrar_pacientes()
-        
-    elif opcion == "7":
-        hospital.mostrar_pacientes_menores()
-        
-    elif opcion == "8":
-        hospital.mostrar_pacientes_mayores()
-        
-    elif opcion =="9":
-        print("\nHasta luego")
+        print("Hasta luego")
         break
-
+    
